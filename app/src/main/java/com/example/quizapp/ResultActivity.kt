db73f5ba3a61cc86e993279d53f566c0b856e299
molecule03 <laveshsalaskar619@gmail.com>
2023-07-10 23:41:45 +0530

    Documentation Added

--- app/src/main/java/com/example/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -1,3 +1,4 @@
+
 package com.example.quizapp
 
 import android.annotation.SuppressLint
@@ -8,29 +9,34 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
+/**
+ * This activity displays the quiz result with the user's score.
+ */
 class ResultActivity : AppCompatActivity() {
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        // Hide the status bar for a fullscreen experience
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        val tv_userName : TextView = findViewById(R.id.tv_userName)
-        val tv_score : TextView = findViewById(R.id.tv_score);
-        val btn_finish : Button = findViewById(R.id.btn_finish)
+        // Get references to UI elements
+        val tvUserName: TextView = findViewById(R.id.tv_userName)
+        val tvScore: TextView = findViewById(R.id.tv_score)
+        val btnFinish: Button = findViewById(R.id.btn_finish)
 
+        // Set the user name and score
+        tvUserName.text = intent.getStringExtra(Constants.userName)
+        tvScore.text =
+            "Your Score is ${intent.getStringExtra(Constants.correctAns)} out of ${intent.getStringExtra(Constants.totalQuestions)}"
 
-        tv_userName.text = intent.getStringExtra(Constants.userName);
-        tv_score.text = "Your Score is ${intent.getStringExtra(Constants.correctAns)} out of ${
-            intent.getStringExtra(Constants.totalQuestions)} ";
-
-        btn_finish.setOnClickListener{
+        // Set up click listener for the finish button
+        btnFinish.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
-
-
     }
 }
