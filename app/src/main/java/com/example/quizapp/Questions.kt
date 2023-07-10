f7c973cdeeb235f64c8a36b993caa0600c9bdda7
molecule03 <laveshsalaskar619@gmail.com>
2023-07-10 23:38:52 +0530

    Documentation Added

--- app/src/main/java/com/example/quizapp/Questions.kt
+++ app/src/main/java/com/example/quizapp/Questions.kt
@@ -1,15 +1,24 @@
 package com.example.quizapp
 
+/**
+ * Data class representing a quiz question.
+ *
+ * @property id The unique identifier of the question.
+ * @property question The text of the question.
+ * @property image The resource ID of the image associated with the question.
+ * @property optionFirst The text of the first option.
+ * @property optionSecond The text of the second option.
+ * @property optionThird The text of the third option.
+ * @property optionFourth The text of the fourth option.
+ * @property correctAns The index of the correct answer (1-based).
+ */
 data class Questions(
-    val id : Int,
-    val question : String,
-    val image : Int,
-    val optionFirst : String,
-    val optionSecond : String,
-    val optionThird : String,
-    val optionFourth : String,
-    val correctAns : Int,
-){
-
-}
-
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionFirst: String,
+    val optionSecond: String,
+    val optionThird: String,
+    val optionFourth: String,
+    val correctAns: Int
+)
